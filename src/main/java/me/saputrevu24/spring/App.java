fc7f529988786c8be3df6c61a8ee08ba1e50373f
Sai Abhilash Putrevu <sputrevu@hawk.iit.edu>
2018-01-19 23:35:29 -0500

    This is my first commit from IntelliJ IDE
    Signed-off-by: Sai Abhilash Putrevu <sputrevu@hawk.iit.edu>

--- src/main/java/me/saputrevu24/spring/App.java
+++ src/main/java/me/saputrevu24/spring/App.java
@@ -6,5 +6,6 @@ package me.saputrevu24.spring;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is a GitHub Project");
     }
 }
